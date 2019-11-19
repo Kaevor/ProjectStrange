9eb03be3aa04a8cd4d6029b26dbaa57842feca49
Kaevor <57923038+Kaevor@users.noreply.github.com>
2019-11-18 21:13:36 -0700

    Initial commit

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool paused = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = TogglePaused();
+        }
+    }
+
+    bool TogglePaused()
+    {
+        if(Time.timeScale == 0.0f)
+        {
+            Time.timeScale = 1.0f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0.0f;
+            return (true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjectStrange
+ First game
